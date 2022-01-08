a26aa28f9976250d592db8ac04614214b0298814
cSDes1gn <christian911@sympatico.ca>
2022-01-08 10:49:40 -0500

    add implementation for bom entry saves

--- src/v1/controllers/boms.ts
+++ src/v1/controllers/boms.ts
@@ -1,7 +1,15 @@
 import { getRepository } from "typeorm";
 import { NextFunction, Request, Response } from "express";
 import { Bom } from "../entities/bom";
-import { ResourceNotFound } from "../util/errors";
+import { BadQuery, HTTPError, ResourceNotFound } from "../util/errors";
+import { RequestBody } from "swagger-jsdoc";
+import { Project } from "../entities/project";
+import { Part } from "../entities/part";
+
+interface BomRequestBody extends RequestBody {
+  part: number;
+  quantity: number;
+}
 
 /**
  * @openapi
@@ -24,6 +32,8 @@ import { ResourceNotFound } from "../util/errors";
 export class BomController {
 
   private bomRepository = getRepository(Bom);
+  private partRepository = getRepository(Part);
+  private projectRepository = getRepository(Part);
 
   /**
    * @openapi
@@ -43,7 +53,7 @@ export class BomController {
    *                 $ref: '#/components/schemas/Bom'
    */
   async all(_request: Request, _response: Response, _next: NextFunction) {
-    return this.bomRepository.find();
+    return this.bomRepository.find({relations: ["part", "project"]});
   }
 
   /**
@@ -73,7 +83,25 @@ export class BomController {
    *         description: Bad request. Request may of failed validation checks.
    */
   async save(request: Request, _response: Response, _next: NextFunction) {
-    return this.bomRepository.save(request.body);
+    // compute and store netprice
+    let unitPrice: number;
+    let result: Bom;
+    try {
+      const part = await this.partRepository.findOne((request.body as BomRequestBody).part, {select: ["unit_price"]});
+      unitPrice = part? part.unit_price: 0;
+    } catch (err) {
+      throw new BadQuery("Error while retrieving part unit price for net price calculation");
+    }
+    (request.body as BomRequestBody).net_price = (request.body as BomRequestBody).quantity * unitPrice;
+    // perform save query operation
+    try {
+      result = await (this.bomRepository.save(request.body) as Promise<Bom>);
+    } catch (err) {
+      throw new HTTPError((err as Error).message);
+    }
+    result.part = await (this.partRepository.findOne(result.part) as Promise<Part>);
+    result.project = await (this.projectRepository.findOne(result.project) as Promise<Project>);
+    return result;
   }
 
   /**
