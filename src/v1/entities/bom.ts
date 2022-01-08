4998b53bfc40cba0436324b9b4d3245161ff734f
cSDes1gn <christian911@sympatico.ca>
2022-01-07 23:35:27 -0500

    fix for bom fields and composite pkeys

--- src/v1/entities/bom.ts
+++ src/v1/entities/bom.ts
@@ -6,7 +6,8 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   ManyToMany,
-  OneToMany
+  OneToMany,
+  PrimaryColumn
 } from "typeorm";
 import { Project } from "./project";
 import { Part } from "./part";
@@ -29,9 +30,15 @@ import { Part } from "./part";
  *         part:
  *           $ref: '#/components/schemas/Part'
  *           description: Part association
+ *         quantity:
+ *           type: integer
+ *           minimum: 1
+ *           description: quantity of part for project
+ *           example: 5
  *         net_price:
  *           type: double
- *           minimum: 0
+ *           minimum: 0.01
+ *           description: product of part unit price and quantity
  *           example: 34.08
  *         revision:
  *           type: string
@@ -49,15 +56,15 @@ import { Part } from "./part";
 @Entity()
 export class Bom extends BaseEntity {
 
+  @PrimaryColumn({ type: 'int', name: 'project_id' })
   @OneToMany(() => Project, (project) => project.id, {
-    primary: true,
     onDelete: 'CASCADE',
     nullable: true
   })
   project: Project;
 
+  @PrimaryColumn({ type: 'int', name: 'part_id' })
   @ManyToMany(() => Part, (part) => part.id, {
-    primary: true,
     onDelete: 'CASCADE',
     nullable: true
   })
@@ -66,10 +73,10 @@ export class Bom extends BaseEntity {
 	@Column({nullable: true})
 	revision: string;
 
-  @Column({type: "decimal", default: 0, precision:2, scale: 5, nullable: false})
+  @Column({type: "decimal", default: 0, precision: 5, scale: 2, nullable: false})
   net_price: number;
 
-  @Column({default: 1, precision: 2, scale: 5, nullable: false})
+  @Column({default: 1, nullable: false})
   quantity: number;
 
   @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)" })
