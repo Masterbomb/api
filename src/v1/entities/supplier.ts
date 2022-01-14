15d208bd5ccf0a3462f97571d983eb372885bf82
cSDes1gn <christian911@sympatico.ca>
2022-01-14 01:53:07 -0500

    add openapi doc

--- src/v1/entities/supplier.ts
+++ src/v1/entities/supplier.ts
@@ -1,5 +1,30 @@
 import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Supplier:
+ *       type: object
+ *       required:
+ *         - name
+ *         - website
+ *       properties:
+ *         id:
+ *           type: number
+ *           required: true
+ *           description: The auto-generated id of the supplier
+ *         name:
+ *           type: string
+ *           description: The supplier name
+ *         website:
+ *           type: string
+ *           description: The supplier webpage
+ *       example:
+ *         id: 1
+ *         name: Digikey
+ *         website: https://www.digikey.com/
+ */
 @Entity()
 export class Supplier extends BaseEntity {
 	@PrimaryGeneratedColumn()
