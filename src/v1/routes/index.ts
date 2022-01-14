f93fb5a5b6e78802875a31138bd3ada6da1d9a1d
cSDes1gn <christian911@sympatico.ca>
2022-01-14 15:20:16 -0500

    add manufacturer routes to master route array

--- src/v1/routes/index.ts
+++ src/v1/routes/index.ts
@@ -1,4 +1,5 @@
 import { supplierRoutes } from "./suppliers";
+import { manufacturerRoutes } from "./manufacturers";
 import { Route } from "./interfaces";
 
-export const routes: Route[] = supplierRoutes;
+export const routes: Route[] = supplierRoutes.concat(manufacturerRoutes);
