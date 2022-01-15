bde1341f4441a7c4ae41deb07afebdd461dccf8a
cSDes1gn <christian911@sympatico.ca>
2022-01-15 02:49:41 -0500

    add parts routes

--- src/v1/routes/index.ts
+++ src/v1/routes/index.ts
@@ -1,5 +1,9 @@
 import { supplierRoutes } from "./suppliers";
 import { manufacturerRoutes } from "./manufacturers";
 import { Route } from "./interfaces";
+import { partRoutes } from "./parts";
 
-export const routes: Route[] = supplierRoutes.concat(manufacturerRoutes);
+export const routes: Route[] = supplierRoutes.concat(
+  manufacturerRoutes,
+  partRoutes
+);
