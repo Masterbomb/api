322e4ddc3ac0e2c54cf42b22ce5cada66eb9ee5e
cSDes1gn <christian911@sympatico.ca>
2022-01-15 02:49:57 -0500

    add parts route and custom foreign key validation

---
+++ src/v1/routes/parts.ts
@@ -0,0 +1,65 @@
+import { body, param, CustomValidator } from "express-validator";
+import { PartController } from "../controllers/parts";
+import { Route, Queries, HTTPRequests } from "./interfaces";
+import { getRepository } from "typeorm";
+import { Supplier } from "../entities/supplier";
+import { Manufacturer } from "../entities/manufacturer";
+
+
+const isValidSupplier: CustomValidator = async (id:number) => {
+  await getRepository(Supplier).findOne(id).then(user => {
+    if (!user) {
+      return Promise.reject('Supplier does not exist');
+    }
+    return Promise.resolve('Supplier exists');
+  });
+};
+
+const isValidManufacturer: CustomValidator = async (id:number) => {
+  await getRepository(Manufacturer).findOne(id).then(user => {
+    if (!user) {
+      return Promise.reject('Manufacturer does not exist');
+    }
+    return Promise.resolve('Manufacturer exists');
+  });
+};
+
+export const partRoutes: Route[] = [
+  {
+    method: HTTPRequests.get,
+    path: "/parts",
+    controller: PartController,
+    action: Queries.all,
+    validation: [],
+  },
+  {
+    method: HTTPRequests.get,
+    path: "/parts/:id",
+    controller: PartController,
+    action: Queries.one,
+    validation: [
+      param('id').isInt(),
+    ]
+  },
+  {
+    method: HTTPRequests.post,
+    path: "/parts",
+    controller: PartController,
+    action: Queries.save,
+    validation: [
+      body('name').isString(),
+      body('manufacturer').custom(isValidManufacturer),
+      body('supplier').custom(isValidSupplier),
+      body('unit_price').isNumeric(),
+    ],
+  },
+  {
+    method: HTTPRequests.delete,
+    path: "/parts/:id",
+    controller: PartController,
+    action: Queries.remove,
+    validation: [
+      param('id').isInt(),
+    ],
+  }
+];
